69938feba1aa40a8f997551aec9af564d2bba6ec
Md. Al-Amin <41773444+alamin22@users.noreply.github.com>
2019-07-29 00:11:13 -0700

    HTML file

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu Dropdown With CSS3</title>
+   <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <div class="menubar">
+  	<ul>
+  		<li><a href="#">HOME</a></li>
+  		<li><a href="#">LEARN</a>
+           <ul>
+           	 <li><a href="#">PHP</a></li>
+           	 <li><a href="#">Laravel</a>
+               <ul>
+               	<li><a href="#">5</a></li>
+               	<li><a href="#">5.2</a></li>
+               	<li><a href="#">5.4</a></li>
+               	<li><a href="#">5.5</a></li>
+               	<li><a href="#">5.8</a></li>
+               </ul>
+
+           	 </li>
+           	 <li><a href="#">JAVA</a></li>
+           	 <li><a href="#">ASP.NET</a></li>
+           	 <li><a href="#">JavaScript</a></li>
+           	 <li><a href="#">CSS</a></li>
+           	 <li><a href="#">DataBase</a>
+           	   <ul>
+               	<li><a href="#">MySQL</a></li>
+               	<li><a href="#">ORACLE</a></li>
+               	<li><a href="#">Dbase</a></li>
+               	<li><a href="#">MicroSoft Access</a></li>
+               </ul>
+           	 </li>
+
+           </ul>
+  		</li>	
+  		<li><a href="#">ABOUT</a></li>
+  	     <li><a href="#">USER</a>
+           <ul>
+           	<li><a href="#">Sign Up</a></li>
+           	<li><a href="#">Sign In</a></li>
+           </ul>
+
+  		</li>		
+  	</ul>
+
+  </div>
+</body>
+</html>
